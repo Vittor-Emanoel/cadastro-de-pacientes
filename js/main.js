9c1c18287ea75e3e0f719509498991828d9333a4
vittor-Emanoel <vittoremanoel1234@gmail.com>
2022-08-05 17:08:10 -0300

    Formulario para adicionar os pacientes

--- js/main.js
+++ js/main.js
@@ -3,6 +3,13 @@ titulo.textContent = "Aparecida Nutricionista";
 
 var pacientes = document.querySelectorAll(".paciente");
 
+var botao = document.querySelector('#adicionar-paciente');
+
+botao.addEventListener('click',function(){
+ console.log('add paciente');
+});
+
+
 //ele só para quando for menor que 0 pacientes.
 for (let i = 0; i < pacientes.length; i++) {
   var paciente = pacientes[i]; // armazenando todos os paciente dentro de uma array. array pacientes e esse array está dentro da variavel paciente.
@@ -18,9 +25,11 @@ for (let i = 0; i < pacientes.length; i++) {
   var pesoEhValido = true;
   var AlturaEhValida = true;
 
-  //Teste logica para saber se o peso e a altura são validas.
+//Teste logica para saber se o peso e a altura são validas.
 
 //adicionando estilos pela classe css. - alterando o aspecto visual.
+//tem duas formas de manipular os styles em Java script.
+// 1- style.color = color || criando uma classe css
   if (peso <= 0 || peso >= 1000) {
     tdImc.textContent = "Informações Inválidas!";
     paciente.classList.add('paciente-invalido');
@@ -36,6 +45,25 @@ for (let i = 0; i < pacientes.length; i++) {
   //Não tem teste === pq ele já comeca como verdadeiro
   if (pesoEhValido && AlturaEhValida) {
     var imc = peso / (altura * altura);
-    tdImc.textContent = imc.toFixed(2); //etoFixed limita as casas decimas
+    tdImc.textContent = imc.toFixed(2); //.toFixed limita as casas decimas 
   }
 }
+
+
+
+
+
+/*
+ESSES SÃO OS DOIS JEITOS DE ADD EVENTOS
+
+FUNÇÃO DECLARATIVA OU ANONIMA
+
+titulo.addEventListener('click', mostraMensagem);
+
+function mostraMensagem() {
+  return console.log('Olá, eu fui clicado!');
+}
+
+titulo.addEventListener('click', function(){
+  console.log('oláa')
+})*/ 
