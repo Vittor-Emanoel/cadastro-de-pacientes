4c41a34cafa5b80bae0804b10b80b2f930de09e1
vittor-Emanoel <vittoremanoel1234@gmail.com>
2022-08-05 13:16:44 -0300

    validação e o calculo do IMC para todos os pacientes

--- js/main.js
+++ js/main.js
@@ -1,45 +1,41 @@
-var titulo = document.querySelector('h1');
-titulo.textContent = 'Aparecida Nutricionista';
+var titulo = document.querySelector("h1");
+titulo.textContent = "Aparecida Nutricionista";
 
+var pacientes = document.querySelectorAll(".paciente");
 
-var paciente = document.querySelector("#primeiro");
-var tdPeso = paciente.querySelector(".info-peso");
+//ele só para quando for menor que 0 pacientes.
+for (let i = 0; i < pacientes.length; i++) {
+    
+  var paciente = pacientes[i]; // armazenando todos os paciente dentro de uma array. array pacientes e esse array está dentro da variavel paciente.
 
-var peso = tdPeso.textContent;
+  var tdPeso = paciente.querySelector(".info-peso");
+  var peso = tdPeso.textContent;
 
-var tdAltura = paciente.querySelector('.info-altura');
-var altura = tdAltura.textContent;
+  var tdAltura = paciente.querySelector(".info-altura");
+  var altura = tdAltura.textContent;
 
-var tdImc = paciente.querySelector('.info-imc');
+  var tdImc = paciente.querySelector(".info-imc");
 
+  var pesoEhValido = true;
+  var AlturaEhValida = true;
 
+  //Teste logica para saber se o peso e a altura são validas.
 
-var pesoEhValido = true;
-var AlturaEhValida = true;
-
-//Teste logica para saber se o peso e a altura são validas.
-
-if(peso <= 0 || peso >= 1000) {
-    tdImc.textContent = 'Informações Inválidas!';
-    tdImc.style.color = 'red';
+  if (peso <= 0 || peso >= 1000) {
+    tdImc.textContent = "Informações Inválidas!";
+    tdImc.style.color = "red";
     pesoEhValido = false;
-}
-if(altura <= 0 || altura >= 3.00){
-    tdImc.textContent = 'Informações Inválidas!';
-    tdImc.style.color = 'red';
+  }
+  if (altura <= 0 || altura >= 3.0) {
+    tdImc.textContent = "Informações Inválidas!";
+    tdImc.style.color = "red";
     AlturaEhValida = false;
-}
+  }
 
-//teste de validação logico para calcular ou não calcular o IMC
-//Não tem teste === pq ele já comeca como verdadeiro
-if(pesoEhValido && AlturaEhValida){
+  //teste de validação logico para calcular ou não calcular o IMC
+  //Não tem teste === pq ele já comeca como verdadeiro
+  if (pesoEhValido && AlturaEhValida) {
     var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
+    tdImc.textContent = imc.toFixed(2);//etoFixed limita as casas decimas
+  }
 }
-
-
-
-
-
-
-
