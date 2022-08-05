0d6cee827f65fb8567f62ca7788a7b1002e56990
vittor-Emanoel <vittoremanoel1234@gmail.com>
2022-08-05 11:52:19 -0300

    Válidação com operadores lógicos

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 var titulo = document.querySelector('h1');
-
 titulo.textContent = 'Aparecida Nutricionista';
 
 
@@ -13,14 +12,34 @@ var altura = tdAltura.textContent;
 
 var tdImc = paciente.querySelector('.info-imc');
 
-var imc = peso / (altura * altura);
-
-tdImc.textContent = imc;
 
 
-console.log(peso);
-console.log(altura);
-console.log(imc);
+var pesoEhValido = true;
+var AlturaEhValida = true;
+
+//Teste logica para saber se o peso e a altura são validas.
+
+if(peso <= 0 || peso >= 1000) {
+    tdImc.textContent = 'Informações Inválidas!';
+    tdImc.style.color = 'red';
+    pesoEhValido = false;
+}
+if(altura <= 0 || altura >= 3.00){
+    tdImc.textContent = 'Informações Inválidas!';
+    tdImc.style.color = 'red';
+    AlturaEhValida = false;
+}
+
+//teste de validação logico para calcular ou não calcular o IMC
+//Não tem teste === pq ele já comeca como verdadeiro
+if(pesoEhValido && AlturaEhValida){
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
+
+
+
+
 
 
 
